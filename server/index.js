c077d529b6172526de91f9e26be3f1e94537e422
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-10-17 16:11:03 -0500

    Completed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+// Node uses require to import packages. Here we bring in express and save it to a variable.
 const bodyParser = require('body-parser');
+// We bring in the package body-parser that we will use to convert the incoming request's body to a js object.
 const mc = require('./controllers/messages_controller');
 
 const app = express ();
+// We name our express application app. We creat a new application by invoking express.
 
 app.use( bodyParser.json () );
 app.use( express.static( __dirname + '/../public/build'));
@@ -14,4 +17,5 @@ app.put ( `${messagesBaseUr1}/:id`, mc.update);
 app.delete (`${messagesBaseUr1}/:id`, mc.delete);
 
 const port = 3001;
+// We define the port that our server will run on. We will use it a bit later in our server.
 app.listen ( port, () => { console.log(`Server listening on port ${port}`); } );
